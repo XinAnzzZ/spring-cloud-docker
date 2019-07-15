7d28f368fb5c1a70ebc417be125750a3fa0e299b
moore@xingren.com <xinan950806>
2019-07-15 15:45:18 +0800

    deploy

---
+++ demo-service1/deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# 获取当前脚本所在路径，也就是项目根目录
+WORK_PATH=$(dirname $(readlink -f $0));
+
+cd ${WORK_PATH}
+
+git pull --rebase origin master
+
+mvn clean install -Dmaven.test.skip
+
+# 停止并删除容器
+docker stop service-01-api
+docker rm service-01-api
+
+# 删除镜像文件
+docker rmi spring-boot/service-01-api
+
+cd ${WORK_PATH}/service-01-api
+
+# 打包镜像文件
+mvn docker:build
+
+# 启动
+docker run -d -p 9001:9001 --name=service-01-api spring-boot/service-01-api
