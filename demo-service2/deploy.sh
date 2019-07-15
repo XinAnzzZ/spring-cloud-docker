ddaddb26f24305ed113f8425f63743a35046bbf0
moore@xingren.com <xinan950806>
2019-07-15 12:43:03 +0800

    modify d

---
+++ demo-service2/deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# 获取当前脚本所在路径，也就是项目根目录
+WORK_PATH=$(dirname $(readlink -f $0));
+
+cd ${WORK_PATH}
+
+mvn clean install -Dmaven.test.skip
+
+# 停止并删除容器
+docker stop service-02-api
+docker rm service-02-api
+
+# 删除镜像文件
+docker rmi spring-boot/service-02-api
+
+cd ${WORK_PATH}/service-02-api
+
+# 打包镜像文件
+mvn docker:build
+
+# 启动
+docker run -d -p 9002:9002 --name=service-02-api spring-boot/service-02-api
